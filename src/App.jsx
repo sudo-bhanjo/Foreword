b5dfffe2a7a21b304a27e47f261eb254b9e6e0d6
OM SONANI <143808904+om-151@users.noreply.github.com>
2025-07-30 22:41:14 +0530

    aishwarya

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,4 @@
 import './App.css'
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/Navbar'
-import AboutFounder from './pages/AboutFounder/AboutFounder'
-import LogoCarousel from './pages/Carousel/LogoCarousel'
-import Home from './pages/Home/Home'
-// import Programs from './pages/Programs/Programs'
-// import Methodology from './pages/methodology/Methodology'
-import Testimonials from './pages/Testimonials/Testimonials'
-import Section from './pages/Sections/Sections'
-import About from './pages/About/About'
-import MissionAndVision from './pages/MissionandVision/MissionAndVision'
-import Blog from './pages/Blogs/blogs'
-// import IndividualModule from './pages/Individual/Individual'
 import Error from "./pages/InternalServer/InternalServerError"
 
 function App() {
@@ -19,19 +6,6 @@ function App() {
   return (
     <>
       <div className='overflow-x-hidden'>
-        {/* <Navbar />
-        <Home />
-        <About />
-        <MissionAndVision /> */}
-        {/* <Methodology /> */}
-        {/* <Programs /> */}
-        {/* <Section />
-        <Testimonials />
-        <LogoCarousel />
-        <AboutFounder /> */}
-        {/* <IndividualModule /> */}
-        {/* <Blog />
-        <Footer /> */}
         <Error />
       </div>
     </>
